fd4c90f1650a5d7aa98228cd49fa0f01ab251c68
Replit user <>
2022-10-16 10:47:28 +0000

    add

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const index = () => {
+  return (
+    <h1>Wow</h1>
+  )
+}
+
+export default index
--- pages/test.js
+++
@@ -1,7 +0,0 @@
-const test = () => {
-  return (
-    <h1>Hello</h1>
-  )
-}
-
-export default test
